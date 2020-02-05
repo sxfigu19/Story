47cb3845f85b4c66320493ecb15ff86da94d9c69
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-02-05 14:32:42 -0500

    Update story.py

---
+++ story.py
@@ -0,0 +1,34 @@
+def askName():
+    answer = input("To enter Story Mode, type 'New Game'.")
+    if answer == "New Game":
+        print("< Entering Story Mode >")
+    else:
+        print("< Error: Wrong Command. Try again. >")
+        askName()
+
+askName()
+
+print("Long ago, there was a legend that if you went down to the old well at midnight on a full moon \n     bringing your most prized possesion, you could unlock a portal to a new world")
+print("")
+print("Rumored that you could start a new life, in a new world, with magical abilities")
+print("")
+input("Press Okay or Enter to continue. ")
+print("")
+
+print("You wake up to someone calling you. ")
+input("???: Hello? Oh you must be the last new villager! Welcome to Asunder. ")
+input("???: My name is Layla. I'm the guide for the new spirits.")
+name = input("Layla: What's your name young one? ")
+input("You: My name is " + name + ".")
+input("Layla: Nice to meet you " + name + ". Let's take you to meet the others!")
+print("You and Layla walk towards an old tower.")
+print("")
+input("You: What do you mean others?")
+input("Layla: There are many people, such as yourself, whose spirits have been brought here.")
+input("Layla: We were waiting for you so I can help you all find out your powers together.")
+input("Layla: You see here in Asunder, if your spirit has traveled here, then you will be granted magical abilities.")
+input("Layla: and it's my job to help you find them!")
+
+print("")
+print("You and Layla enter the old tower and you notice about five other people who look to be around your age.")
+print("")
