3df18c9a7e309932c042e5d62a97d28448ea9368
Savannah <60404236+sxfigu19@users.noreply.github.com>
2020-02-06 16:01:57 -0500

    Update story.py

--- story.py
+++ story.py
@@ -54,6 +54,7 @@ def intro():
     else:
         print("Asher: Nice to meet you " + name + "! You seem pretty cool, I think we could be great friends.")
 
+    print("")
     input("Layla: Okay everyone, it's very important to understand that you must not take advantage of your powers.")
     print("Layla: Doing so could result in your spirit being sent to Nankan.")
     input("< The whole room fell silent. >")
@@ -71,6 +72,7 @@ def intro():
     input("Asher: So wait does that mean--")
     input("Layla: No more questions, it's getting pretty late and you all need to rest to charge your powers.")
     print("< You end up bunking with Asher and try to sleep >")
+    print("")
 
 
 def firstnight():
@@ -82,9 +84,80 @@ def firstnight():
         print("< You walk out of your room to find Asher in the hallway, he seems to be holding a bag. >")
         print("")
         input("You: Asher? What are you doing?")
-        
+        print("< You scared Asher causing him to almost drop his bag. >")
+        name = input("Asher: Oops! Oh hey, um sorry what was your name again?")
+        input("You: It's " + name + ".")
+        input("Asher: Right, right, " + name + ". My bad.")
+        input("Asher: I was just grabbing a snack, late night munchies.")
+        print("< Asher holds up a bag of potato chips and laughs. >")
+        input("You: ...Um okay. Mind if I have some?")
+        input("Asher: Huh? Oh sure! Take some.")
+        print("< You share a bag of chips with Asher. >")
+        input("Asher: So what do you think of Asunder so far?")
+        chips = input(" A) This place is weird, or B) It seems pretty cool.")
+        if chips == 'A':
+            print("Asher: Yea it's definitely a bit weird at first, but it's not as bad as it seems.")
+        else:
+            print("Asher: Really? Most people find it strange at first. But you're right it's pretty cool here.")
+        print("")
+        input("< You stay up late talking with Asher about Asunder. >")
+        print("")
+        print("")
+
+
+def meetingNaomi():
+    print("< After a long first night, you here a loud crash in the main room. >")
+    input("< You and Asher walk out and find one of the walls with a hole in it. \n   Along with Layla and some other girl who is dressed in battle armour. >")
+    input("Layla: What have I told you about letting your anger getting the best of you?")
+    input("???: I can't help it Layla! \n The Nankan army isn't even playing fair at this point, and we have no way of knowing their plans.")
+    input("Layla: Trust me, I know. But that doesn't mean you can come in and PUNCH MY WALL!")
+    input("???: I'll pay for the damage later, \n but we've been on the same line for weeks and their army is getting stronger and stronger.")
+    input("???: It won't be long until they break the guard walls.")
+    input("Layla: I've heard, but please don't come running into my house like that.")
+    input("Layla: Especially when I have new spirits that--")
+    print("< Layla and the other girl notice you and Asher standing in the hall. >")
+    print("")
+    input("???: Asher? What are you doing here? You should be helping out with the army.")
+    input("Asher: Oh um hey Naomi! I am actually, I came here to find out which one of the new spirts could help out.")
+    input("Asher: And I think I've found them!")
+    print("< Asher points at you. >")
+    print("")
+    input("You: Wait what? I don't even have my powers.")
+    input("Asher: Sure you do, it seems like your power charm appeared while you were sleeping.")
+    print("")
+    print("< You look down and see a necklace glowing a bright blue and white. >")
+    input("Naomi: Wow kid, you're pretty powerful.")
+    input("Asher: What? No fair you've got tier V Ice powers and invisibility?! Luckyyyy.")
+    input("Layla: What these two mean is yes, you are powerful. \n Tier V Ice Powers is the second highest ice powers you can have. \n And then invisibility is a pretty rare power too.")
+    input("Layla: Naomi and Asher, can you take them to the training grounds since they've already gained their powers?")
+    print("Naomi: Sure Layla, let's go kid.")
+    name = input("Hey I have a name actually. It's ")
+    input("Naomi: Yea whatever let's get going.")
+    input("Asher: Don't worry " + name + ". Naomi is a little intimidating at first but she's got a nice side somewhere.")
+    input("Naomi: ASHER! You do realize I can hear you right?")
+    input("Asher: Ha... I didn't say anything I swear.")
+    print("")
+
+
+def training():
+    print("< All three of you walk into town into what seems to be an obstacle course. >")
+    input("Naomi: Alright kid, let's see what you got.")
+    input("Asher: To activate your powers, just try and focus on the energy coming from your charm.")
+    power = input("What were the colors of your charm? \n A) Bright Blue and White B) Bright Red and Orange")
+    if power == "A":
+        print("< You feel a cold energy charge through you. \n You then realize your hands contain a blue flame with ice pieces. >")
+    else:
+        print("< Nothing seems to happen. Try again. >")
+        training()
+
+def trainTwo():
+    print("Hello")
 
 
 
 askName()
 intro()
+firstnight()
+meetingNaomi()
+training()
+trainTwo()
